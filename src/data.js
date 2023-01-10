99abf1895dd8fd3b4782675eb1f2b4edf2d68066
DevGustavoMacedo <121763412+DevGustavoMacedo@users.noreply.github.com>
2023-01-10 15:14:09 -0300

    feat: sites data

---
+++ src/data.js
@@ -0,0 +1,56 @@
+const data = [
+  {
+    title: 'Dunder Mifflin',
+    link: 'https://dunder-mifflin.vercel.app/',
+    image: '/dunder-mifflin.png',
+    about:
+      "SSG page for the best sitcom ever! You can view the company's history, file a complaint, view our team and details about each one.",
+    made: [
+      { name: 'NextJS', logo: 'next.js' },
+      { name: 'ReactJS', logo: 'react' },
+      { name: 'Styled--Components', logo: 'styledcomponents' },
+    ],
+  },
+
+    {
+    title: 'Crypto Simulator',
+    link: 'https://cryptosimulator.vercel.app/',
+    image: '/cryptosimulator.png',
+    about:
+      'Simulate buying or selling cryptocurrencies! Choose some coin, the amount, date of entry and exit, and generate a monthly simulation!',
+    made: [
+      { name: 'TypeScript', logo: 'typescript' },
+      { name: 'Vite', logo: 'vite' },
+      { name: 'Styled--Components', logo: 'styledcomponents' },
+      { name: 'ReactJS', logo: 'react' },
+
+    ],
+  },
+
+  {
+    title: 'ToDo List',
+    link: 'https://tsreact-todolist.vercel.app/',
+    image: '/todo-list.png',
+    about:
+      'A classic never gets old! A fully interactive ToDO List, you can add, edit, delete and close your tasks.',
+    made: [
+      { name: 'TypeScript', logo: 'typescript' },
+      { name: 'ReactJS', logo: 'react' },
+    ],
+  },
+
+  {
+    title: 'QrCode ',
+    link: 'https://qrcodecreator.vercel.app',
+    image: '/qrcodecreator.png',
+    about:
+      'Insert the link, choose the color, the background color and create a custom QR Code! You can also download the QRCode image.',
+    made: [
+      { name: 'ReactJS', logo: 'react' },
+      { name: 'Material.UI', logo: 'mui' },
+    ],
+  },
+
+]
+
+export default data
