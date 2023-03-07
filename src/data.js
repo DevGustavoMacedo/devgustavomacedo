f864fe92aa40bd507d95890aee34018bd999e222
DevGustavoMacedo <121763412+DevGustavoMacedo@users.noreply.github.com>
2023-03-07 17:05:21 -0300

    feat(add): dunder mifflin api

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ const data = [
     {
     title: 'Crypto Simulator',
     link: 'https://cryptosimulator.vercel.app/',
-    image: '/cryptosimulator.png',
+    image: '/cryptosimulator.webp',
     about:
       'Simulate buying or selling cryptocurrencies! Choose some coin, the amount, date of entry and exit, and generate a monthly simulation!',
     made: [
@@ -17,7 +17,7 @@ const data = [
   {
     title: 'Dunder Mifflin',
     link: 'https://dunder-mifflin.vercel.app/',
-    image: '/dunder-mifflin.png',
+    image: '/dunder-mifflin.webp',
     about:
       "SSG page for the best sitcom ever! You can view company history, file a complaint, view our team and person details, and listen to our podcast!",
     made: [
@@ -28,9 +28,24 @@ const data = [
   },
 
   {
+    title: 'Dunder Mifflin API',
+    link: 'https://dunder-mifflin-api.vercel.app/',
+    image: '/dunder-mifflin-api.webp',
+    about:
+      "An API to provide data about The Office series characters and Office Ladies podcast episodes",
+    made: [
+      { name: 'nodejs', logo: 'node.js' },
+      { name: 'express', logo: 'express' },
+      { name: 'sequelize', logo: 'sequelize' },
+      { name: 'postgresql', logo: 'postgresql' },
+    ],
+  },
+
+
+  {
     title: 'QrCode Creator',
     link: 'https://qrcodecreator.vercel.app',
-    image: '/qrcodecreator.png',
+    image: '/qrcodecreator.webp',
     about:
       'Insert the link, choose the color, the background color and create a custom QR Code! You can also download the QRCode image.',
     made: [
@@ -42,7 +57,7 @@ const data = [
   {
     title: 'ToDo List',
     link: 'https://tsreact-todolist.vercel.app/',
-    image: '/todo-list.png',
+    image: '/todo-list.webp',
     about:
       'A classic never gets old! A fully interactive ToDO List, you can add, edit, delete and close your tasks.',
     made: [
